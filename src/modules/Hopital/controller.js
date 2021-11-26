685863ff3b8869499862b602190bb9588e95283f
samira <samira@houacines-MacBook-Pro.local>
2021-11-26 16:46:31 +0100

    modules hospital

--- src/modules/Hopital/controller.js
+++ src/modules/Hopital/controller.js
@@ -11,5 +11,22 @@ class HopitalController {
       next(error);
     }
   };
+
+  getAll = async ({ res, next }) => {
+    try {
+      let hopitals = await this.#models.finAll();
+      res.status(200).json(hopitals);
+    } catch (error) {
+      next(error);
+    }
+  };
+  getOne = async ({}) => {
+    try {
+      let hopital = await this.#models.findOne();
+      res.status(200).json(hopital);
+    } catch (error) {
+      nex(error);
+    }
+  };
 }
 export default HopitalController;
